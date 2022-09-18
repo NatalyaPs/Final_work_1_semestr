8f36ce865f95e65d7e74f45e76d6b4d3ddae425f
NatalyaPs <n09051@yandex.ru>
2022-09-18 15:55:19 +0300

    создаем метод PrintArray

--- final_work/finalWork1Sem/Program.cs
+++ final_work/finalWork1Sem/Program.cs
@@ -4,23 +4,33 @@
 либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 лучше обойтись исключительно массивами.*/
 
-string [] array = {"hello", "2", "world", ";-)"};
+string[] array = { "hello", "2", "world", ";-)" };
 //string [] array = {"1234", "1567", "-2", "computer science"};
 //string [] array = {"Russia", "Denmark", "Kazan"};
 
 int size = 0;
-for (int i = 0; i < array.Length; i ++)
+for (int i = 0; i < array.Length; i++)
 {
-    if(array[i].Length <= 3) size++;
+    if (array[i].Length <= 3) size++;
 }
 
-string [] newArray = new string [size];
+string[] newArray = new string[size];
 int j = 0;
-for (int i=0; i < array.Length; i++)
+for (int i = 0; i < array.Length; i++)
 {
-    if(array[i].Length <= 3)
+    if (array[i].Length <= 3)
     {
         newArray[j] = array[i];
         j++;
     }
 }
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + "\t");
+    }
+    Console.WriteLine();
+}
+PrintArray(newArray);
