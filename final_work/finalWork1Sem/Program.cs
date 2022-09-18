96992c04c28ab913aa49a7c62c645f6a310535bb
NatalyaPs <n09051@yandex.ru>
2022-09-18 15:47:29 +0300

    переносим элементы, соотвествующие условию задачи, в новый массив из исходного

--- final_work/finalWork1Sem/Program.cs
+++ final_work/finalWork1Sem/Program.cs
@@ -15,4 +15,12 @@ for (int i = 0; i < array.Length; i ++)
 }
 
 string [] newArray = new string [size];
-
+int j = 0;
+for (int i=0; i < array.Length; i++)
+{
+    if(array[i].Length <= 3)
+    {
+        newArray[j] = array[i];
+        j++;
+    }
+}
